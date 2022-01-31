b3c6ea3fb190e7bf679e9a8f22616d5e59fdb337
gsomer <gasomer75@hotmail.com>
2022-01-30 22:59:41 -0800

    modified form from click to submit and prevent browser default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    // prevents browser from disrupting task entry with refresh
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.classList = "task-item";
     listItemEl.textContent = "Sleep";
@@ -9,4 +12,4 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
