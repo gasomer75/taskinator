12f40b1d4f53ae8a75bea2a3aa6002ca387b7f37
gsomer <gasomer75@hotmail.com>
2022-01-31 00:24:46 -0800

    add task type to list with task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,20 @@ var createTaskHandler = function() {
     // prevents browser from disrupting task entry with refresh
     event.preventDefault();
 
-    var listItemEl = document.createElement("li");
-    listItemEl.classList = "task-item";
-    listItemEl.textContent = "Sleep";
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // append list item to include task type
+    listItemEl.appendChild(taskInfoEl);
+    // append task items to the new task
     tasksToDoEl.appendChild(listItemEl);
 };
 
